4d471c61b8c62de5738fe4f131392d859a991a75
Feirie001 <feirie001@gmail.com>
2021-07-13 14:19:00 +0100

    minor changes

--- server/index.js
+++ server/index.js
@@ -1,3 +1,38 @@
 const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
 
-app
+const app = express();
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+    origin: 'http://localhost:2021',
+}));
+
+app.get('/', (req, res) => {
+    res.json({
+       message: 'Oops, there is no message here'
+    })
+
+});
+  
+app.use((req, res, next) =>{
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);
+    res.json({
+        message: error.message,
+    stack: process.env.NODE.ENV === 'production' ? '' : error.stack,
+    })
+})
+
+const port = process.env.PORT || 2020;
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:2020`)
+});
