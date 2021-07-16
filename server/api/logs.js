9dafd50b918888505ceeb848db8ee12b5bb69bc7
Feirie001 <feirie001@gmail.com>
2021-07-16 04:54:14 +0100

    modified changes

--- server/api/logs.js
+++ server/api/logs.js
@@ -6,13 +6,25 @@ const router = Router();
 
 router.get('/', (req, res)=> {
     res.json({
-        message : 'read about it',
+        message : '🌍',
     });
 });
 
-router.post('/', ( req, res) => {
-    const logEntry = new LogEntry(req.body);
-    console.log(req.body);
+router.post('/', async ( req, res, next) => {
+    try {
+        const logEntry = new LogEntry(req.body);
+        const createdEntry = await logEntry.save();
+        res.json(createdEntry);
+    }
+    catch (error) {
+        if(error.name === 'ValidationError') {
+            res.status(422);
+        }
+        next(error);
+        
+
+    }
+    
 })
 
 module.exports = router;
