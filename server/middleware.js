b3d1186b2af203b3d758fc470f00601a6a105bd2
feirie001@gmail.com <feirie001@gmail.com>
2021-07-20 23:41:59 +0100

    minor changes

--- server/middleware.js
+++ server/middleware.js
@@ -3,6 +3,7 @@ const notFound = (req, res, next) =>{
     const error = new Error(`Not Found - ${req.originalUrl}`);
     res.status(404);
     next(error);
+    applicationCache.CHECKING
 };
  //general error handler(middleware)
 const errorHandler = (error, req, res, next) => {
